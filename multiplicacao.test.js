a758e015f3b76410befeb7f506ae880860f155a0
samuel.sousa.2009 <samuel.sousa.2009@aluno.edu.pi.gov.br>
2026-06-10 12:45:37 +0000

    atividade aula DS

--- multiplicacao.test.js
+++ multiplicacao.test.js
@@ -1,4 +1,19 @@
 const multiplicacao = require('./multiplicacao');
+
+
 test ('Deve multiplicar dois numeros', () => {
     expect(multiplicacao(20, 3)).toBe(60);
 });
+
+test ('Deve multiplicar dois números negativos', () => {
+    expect(multiplicacao(-5, -2)).toBe(10);
+});
+
+test ('Deve multiplicar um positivo e negativo', () => {
+    expect(multiplicacao(-4, 6)).toBe(-24);
+});
+
+test ('Deve multiplicar dos numeros e falhar', () => {
+    expect(multiplicacao(3, 2)).toBe(5);
+});
+
