8a5900dbd4838706e86e4bbd433655141c4607e6
samuel.sousa.2009 <samuel.sousa.2009@aluno.edu.pi.gov.br>
2026-06-16 17:53:32 +0000

    aula DS sobre objetos em js

---
+++ objeto.js
@@ -0,0 +1,19 @@
+const usuario = {
+    nome : "Samuel",
+    idade : 28,
+    altura : 1.81,
+    sexo : "Masculino",
+    peso : 78,
+    passatempo : "andar de bike"
+}
+
+console.log(usuario.nome);
+console.log(usuario.idade);
+console.log(usuario.altura);
+console.log(usuario.sexo);
+console.log(usuario.peso);
+console.log(usuario.passatempo);
+
+usuario.idade = 17;
+
+console.log(usuario.idade)
